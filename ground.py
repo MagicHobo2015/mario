1183bcd0f7f53fbf9c443cd02249eee9d997d195
Dracosop <trungnguyencal2@gmail.com>
2023-03-19 03:54:19 -0700

    Forgot to add ground.py

---
+++ ground.py
@@ -0,0 +1,29 @@
+# For parameter type hinting only
+from __future__ import annotations
+from pytmx.pytmx import TiledTileLayer
+
+import pygame as pg
+from vector import Vector
+
+class Ground(pg.sprite.Sprite):
+    def __init__(self, game: Game, layer: TiledTileLayer):
+        super().__init__()
+        self.game = game
+        self.tile_size = game.settings.tile_size
+        self.layer = layer
+        self.layer_vel = Vector()
+        print(self.layer)
+
+        for x, y, sprite in self.layer.tiles():
+            print(f"{x, y, sprite}")
+
+    def update(self):
+        self.layer.offsetx += self.layer_vel.x
+        self.layer.offsety += self.layer_vel.y
+        self.draw()
+
+    def draw(self):
+        # Draws all the tiles in the layer object based on the x and y coordinates
+        # Also adds the offset to the x and y coordinates (from moving left or right)
+        for x, y, sprite in self.layer.tiles():
+            self.game.screen.blit(sprite, (x * self.tile_size + self.layer.offsetx, y * self.tile_size + self.layer.offsety))
